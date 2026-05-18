d9476e86557650d04b07b9f540d6dcd52609b013
camille <j22036387@student.newinti.edu.my>
2026-05-18 18:47:33 +0800

    Refactor bar chart

--- Exercise 5/js/barchart55.js
+++ Exercise 5/js/barchart55.js
@@ -1,39 +1,54 @@
-d3.csv("data/Ex5_TV_energy_55inchtv_byScreenType.csv").then(data => {
-    const valueKey = "Mean(Labelled energy consumption (kWh/year))";
-    data.forEach(d => d.value = +d[valueKey]);
+window.initExercise5BarChart55 = function() {
+    const containerId = "#ex5-barchart55";
+    const containerNode = d3.select(containerId).node();
+    if (!containerNode) return;
 
-    const container = d3.select("#bar-chart-container");
-    const margin = {top: 20, right: 20, bottom: 40, left: 50};
-    const width = 500 - margin.left - margin.right;
-    const height = 350 - margin.top - margin.bottom;
+    d3.select(containerId).selectAll("*").remove();
 
-    const svg = container.append("svg")
-        .attr("viewBox", `0 0 500 350`)
-        .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
+    const width = containerNode.getBoundingClientRect().width || 400;
+    const height = 360; 
+    const margin = { top: 25, right: 20, bottom: 40, left: 60 };
 
-    const xScale = d3.scaleBand()
-        .domain(data.map(d => d.Screen_Tech))
-        .range([0, width])
-        .padding(0.3);
+    const svg = d3.select(containerId)
+        .append("svg")
+        .attr("width", "100%")
+        .attr("height", "100%")
+        .attr("viewBox", `0 0 ${width} ${height}`)
+        .attr("preserveAspectRatio", "xMinYMin meet");
 
-    const yScale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d.value)])
-        .range([height, 0]);
+    d3.csv("data/Ex5_TV_energy_55inchtv_byScreenType.csv").then(data => {
+        data.forEach(d => {
+            d.value = +d["Mean(Labelled energy consumption (kWh/year))"];
+            d.type = d.Screen_Tech;
+        });
 
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(xScale));
+        const xScale = d3.scaleBand()
+            .domain(data.map(d => d.type))
+            .range([margin.left, width - margin.right])
+            .padding(0.45);
 
-    svg.append("g").call(d3.axisLeft(yScale));
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.value) * 1.15])
+            .range([height - margin.bottom, margin.top]);
 
-    svg.selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", d => xScale(d.Screen_Tech))
-        .attr("y", d => yScale(d.value))
-        .attr("width", xScale.bandwidth())
-        .attr("height", d => height - yScale(d.value))
-        .attr("fill", "#1565c0");
-});
+        svg.append("g")
+            .attr("transform", `translate(0, ${height - margin.bottom})`)
+            .call(d3.axisBottom(xScale));
+
+        svg.append("g")
+            .attr("transform", `translate(${margin.left}, 0)`)
+            .call(d3.axisLeft(yScale));
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", d => xScale(d.type))
+            .attr("y", d => yScale(d.value))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => height - margin.bottom - yScale(d.value))
+            .attr("fill", "#ff6600")
+            .attr("stroke", "#cc5200")
+            .attr("stroke-width", "1px");
+    });
+};
